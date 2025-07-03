3fc583baeacf27407c978afdb73b0be87ae36cb6
mouadtouhafi <touhafimouad@gmail.com>
2025-07-03 23:54:58 +0100

    Using enhanced for loop instead of traditionnal for loop.

--- src/main/java/ExpleoJobCollector.java
+++ src/main/java/ExpleoJobCollector.java
@@ -20,29 +20,31 @@ public class ExpleoJobCollector {
              * This 4th iframe contains the dynamically loaded job listings.
              */
             wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(4));
-            WebElement jobTable = wait.until(
+            WebElement jobTable;
+            jobTable = wait.until(
                     ExpectedConditions.visibilityOfElementLocated(
                             By.cssSelector("div.container-fluid.iCIMS_JobsTable")
                     )
             );
             List<WebElement> rows = jobTable.findElements(By.className("row"));
 
-            for (int i = 0; i < rows.size(); i++) {
+            for (WebElement row : rows) {
                 //System.out.println("Row " + (i + 1) + ": " + rows.get(i).getText());
-                WebElement job = rows.get(i).findElement(By.cssSelector("div.col-xs-12.title a.iCIMS_Anchor h3"));
+                WebElement job = row.findElement(By.cssSelector("div.col-xs-12.title a.iCIMS_Anchor h3"));
                 String jobTitle = job.getText();
 
                 /*
-                * The four DIVs inside the element ".col-xs-12 additionalFields" have a repetitive structure,
-                * for that we used the term ":nth-of-type(4)" which indicates which block number we wanna get.
-                * */
-                String location = rows.get(i).findElement(By.cssSelector("div.col-xs-12.additionalFields > dl > div.iCIMS_JobHeaderTag:nth-of-type(1) dd.iCIMS_JobHeaderData span")).getText();
-                String contractType = rows.get(i).findElement(By.cssSelector("div.col-xs-12.additionalFields > dl > div.iCIMS_JobHeaderTag:nth-of-type(3) dd.iCIMS_JobHeaderData span")).getText();
-                String workMode = rows.get(i).findElement(By.cssSelector("div.col-xs-12.additionalFields > dl > div.iCIMS_JobHeaderTag:nth-of-type(4) dd.iCIMS_JobHeaderData span")).getText();
+                 * The four DIVs inside the element ".col-xs-12 additionalFields" have a repetitive structure,
+                 * for that we used the term ":nth-of-type(4)" which indicates which block number we wanna get.
+                 * */
+                String location = row.findElement(By.cssSelector("div.col-xs-12.additionalFields > dl > div.iCIMS_JobHeaderTag:nth-of-type(1) dd.iCIMS_JobHeaderData span")).getText();
+                String contractType = row.findElement(By.cssSelector("div.col-xs-12.additionalFields > dl > div.iCIMS_JobHeaderTag:nth-of-type(3) dd.iCIMS_JobHeaderData span")).getText();
+                String workMode = row.findElement(By.cssSelector("div.col-xs-12.additionalFields > dl > div.iCIMS_JobHeaderTag:nth-of-type(4) dd.iCIMS_JobHeaderData span")).getText();
 
                 /* Here we access the job link to collect the job requirements */
                 job.click();
                 System.out.println(jobTitle + " - " + location + " - " + contractType + " - " + workMode);
+
             }
 
         } catch (Exception e) {
