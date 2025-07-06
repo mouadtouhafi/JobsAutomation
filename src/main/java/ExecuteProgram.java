10efb2f7bc309a60638bb27d016da9f22bba78f2
mouadtouhafi <touhafimouad@gmail.com>
2025-07-06 01:55:02 +0100

    Creating the file.

---
+++ src/main/java/ExecuteProgram.java
@@ -0,0 +1,32 @@
+import collectjobs.ExpleoJobCollector;
+import dataorganize.DataToExcel;
+
+public class ExecuteProgram {
+    public static void main(String[] args) {
+
+        ExpleoJobCollector expleoJobCollector = new ExpleoJobCollector();
+        expleoJobCollector.setUpDriver();
+        expleoJobCollector.getJobsInformations();
+        expleoJobCollector.getJobsRequirements();
+        expleoJobCollector.closeDriver();
+
+        System.out.println(" Printing the final Data : ");
+        System.out.println(expleoJobCollector.getId_jobInfo());
+        System.out.println(expleoJobCollector.getJobsLinks());
+        System.out.println(expleoJobCollector.getId_jobMissions());
+        System.out.println(expleoJobCollector.getId_jobQualifications());
+
+
+
+
+        DataToExcel dataToExcel = new DataToExcel();
+        dataToExcel.createWorkbook();
+        dataToExcel.applyBorderStyle();
+        dataToExcel.applyHeaderStyle();
+        dataToExcel.writeTableHeader();
+        dataToExcel.writeData("Expleo", expleoJobCollector.getId_jobInfo(), expleoJobCollector.getJobsLinks(), expleoJobCollector.getId_jobMissions(), expleoJobCollector.getId_jobQualifications());
+
+        dataToExcel.saveWorkbook("C://Users//touhafi//Desktop//output.xlsx");
+
+    }
+}
