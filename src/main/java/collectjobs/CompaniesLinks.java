3e18814494525eef4a94b19092a77b55cd5a9806
mouadtouhafi <touhafimouad@gmail.com>
2025-07-07 03:41:15 +0100

    Adding the link for Alten company.

--- src/main/java/collectjobs/CompaniesLinks.java
+++ src/main/java/collectjobs/CompaniesLinks.java
@@ -2,4 +2,5 @@ package collectjobs;
 
 public class CompaniesLinks {
     public final String expleoLink = "https://expleo-jobs-ma-fr.icims.com/jobs/search";
+    public final String altenLink = "https://www.alten.ma/?utm_source=ALTEN.com%20career%20jobboard&utm_medium=Dropdown%20career%20jobboard%20ALTEN.com&utm_campaign=Dropdown%20Career%20ALTEN.com#rejoignez-nous";
 }
